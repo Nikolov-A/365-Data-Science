45607d8c9d8f0eb7671b9f0762d6bc2437fc0d9e
Nikolov-A <aleksandar.nikolov092@gmail.com>
2021-08-15 11:37:41 +0200

    15/08/2021

---
+++ 1. SQL/6. MySQL Aggregate Functions/COUNT_FL.sql
@@ -0,0 +1,29 @@
+USE employees;
+
+SELECT 
+    *
+FROM
+    salaries
+ORDER BY salary DESC
+LIMIT 10;
+
+SELECT 
+    COUNT(salary)
+FROM
+    salaries;
+    
+-- How many employee start dates are in the database? -- 
+
+SELECT 
+    COUNT(DISTINCT from_date)
+FROM
+    salaries;
+    
+-- Counting NULL values `` 
+
+SELECT 
+    COUNT(*)
+FROM
+    salaries;
+    
+    
